1cdeaac21c0f41cd9a694ac7f61efc092f66ce8d
Alexandra <thedatacurious@gmail.com>
2021-08-04 12:56:48 +0800

    initial commit for streamgraph project

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const margin = {top: 100, right: 20, bottom: 50, left: 50};
+const width = 1160;
+const height = 600;
+const groups = [
+  { key: 'nominees_caucasian', label: 'caucasian or another', color: '#EFC7C2' },
+  { key: 'nominees_afrodescendant', label: 'afrodescendant', color: '#68A691' },
+  { key: 'nominees_hispanic', label: 'hispanic', color: '#694F5D' },
+  { key: 'nominees_asian', label: 'asian', color: '#BFD3C1' },
+];
+
+// Load the data here
+
+// Create your visualization here
+const createViz = () => {};
