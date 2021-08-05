1f4fd9b750f4962c0b7ae76cd4e73ef06dd53ed9
Alexandra <thedatacurious@gmail.com>
2021-08-05 14:13:34 +0800

    formatted data for viz

--- js/main.js
+++ js/main.js
@@ -8,16 +8,47 @@ const groups = [
   { key: 'nominees_asian', label: 'asian', color: '#BFD3C1' },
 ];
 
+  let dataFormatted = [];
+
 // Load the data here
 
+
+
 const dataset = d3.csv('./data/academy_awards_nominees.csv').then(d=> {
-  const ethnicBreakdown = d3.rollups(d, v => d3.count(v, d=> d.ethnic_background), d=> d.year)
-  // const totalNominees = d3.rollup(d, v => d3.sum(v.length, d => d.ethnic_background), d => d.year)
-  console.log(groups.map(d => d.color))
+
+
+  const totalNominees = d3.rollup(d, v => v.length, d => d.year, d => d.ethnic_background).entries();
+
+  for (let i of totalNominees){
+    const year = new Date(parseInt(i[0]),0).getFullYear();
+    const nominees_caucasian = i[1].get('') || 0;
+    const nominees_afrodescendant = i[1].get('black') || 0;
+    const nominees_hispanic = i[1].get('hispanic') || 0;
+    const nominees_asian = i[1].get('asian') || 0;
+    const nominees_total = nominees_caucasian + nominees_afrodescendant + nominees_hispanic + nominees_asian;
+
+    let entryFormatted = new Nominees(year,nominees_total, nominees_caucasian, nominees_afrodescendant,nominees_hispanic,nominees_asian)
+    dataFormatted.push(entryFormatted)
+
+    // console.log(year,nominees_total, nominees_caucasian, nominees_afrodescendant,nominees_hispanic,nominees_asian)
+  }
+
+
 })
 
+function Nominees(year,nominees_total,nominees_caucasian, nominees_afrodescendant,nominees_hispanic,nominees_asian){
+  this.year = year;
+  this.nominees_total = nominees_total;
+  this.nominees_caucasian = nominees_caucasian;
+  this.nominees_afrodescendant = nominees_afrodescendant;
+  this.nominees_hispanic = nominees_hispanic;
+  this.nominees_asian = nominees_asian;
+}
+
+
+
 // Create your visualization here
-const createViz = () => {
+const createViz = (dataFormatted) => {
   // Set chart dimensions
   let dimensions = {
     width: 1160,
@@ -42,7 +73,7 @@ const createViz = () => {
 
   const colorScale = d3.scaleOrdinal().domain(groups.map(d=> d.key)).range(groups.map(d => d.color))
 
-  const timeScale = d3.scaleTime().domain(d3.extent(dataFormated.year, d => new Date(parseInt(d.year), 0))).range([0,dimensions.boundedWidth])
+  const timeScale = d3.scaleTime().domain(d3.extent(dataFormatted.map(d=> d.year))).range([0,dimensions.boundedWidth])
 
   const areaStack = d3.stack()
   .keys([groups])
