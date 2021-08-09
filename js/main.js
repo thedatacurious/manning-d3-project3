d71c8a61f21e47bd216852b9694b8f512b4f4b9c
Alexandra <thedatacurious@gmail.com>
2021-08-09 21:01:11 +0800

    drew streamgraph

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-const margin = {top: 100, right: 20, bottom: 50, left: 50};
-const width = 1160;
-const height = 600;
+// const margin = {top: 100, right: 20, bottom: 50, left: 50};
+// const width = 1160;
+// const height = 600;
 const groups = [
-  { key: 'nominees_caucasian', label: 'caucasian or another', color: '#EFC7C2' },
-  { key: 'nominees_afrodescendant', label: 'afrodescendant', color: '#68A691' },
-  { key: 'nominees_hispanic', label: 'hispanic', color: '#694F5D' },
-  { key: 'nominees_asian', label: 'asian', color: '#BFD3C1' },
+  { key: 'nominees_caucasian', label: 'caucasian or another', color: '#9BC4CB' },
+  { key: 'nominees_afrodescendant', label: 'afrodescendant', color: '#BDA0BC' },
+  { key: 'nominees_hispanic', label: 'hispanic', color: '#A2708A' },
+  { key: 'nominees_asian', label: 'asian', color: '#0B4F6C' },
 ];
 
   let dataFormatted = [];
 
+  // Create your visualization here
+async function createViz(){
+
 // Load the data here
 
 
-
-const dataset = d3.csv('./data/academy_awards_nominees.csv').then(d=> {
-
-
-  const totalNominees = d3.rollup(d, v => v.length, d => d.year, d => d.ethnic_background).entries();
+  const dataset = await d3.csv('./data/academy_awards_nominees.csv')
+  const totalNominees = d3.rollup(dataset, v => v.length, d => d.year, d => d.ethnic_background).entries();
 
   for (let i of totalNominees){
     const year = new Date(parseInt(i[0]),0).getFullYear();
@@ -34,7 +34,7 @@ const dataset = d3.csv('./data/academy_awards_nominees.csv').then(d=> {
   }
 
 
-})
+
 
 function Nominees(year,nominees_total,nominees_caucasian, nominees_afrodescendant,nominees_hispanic,nominees_asian){
   this.year = year;
@@ -45,10 +45,6 @@ function Nominees(year,nominees_total,nominees_caucasian, nominees_afrodescendan
   this.nominees_asian = nominees_asian;
 }
 
-
-
-// Create your visualization here
-const createViz = (dataFormatted) => {
   // Set chart dimensions
   let dimensions = {
     width: 1160,
@@ -66,19 +62,47 @@ const createViz = (dataFormatted) => {
 
   const wrapper = d3.select('#viz')
   .append('svg')
-  .attr('width', dimensions.boundedWidth)
-  .attr('height', dimensions.boundedHeight)
+  .attr('width', dimensions.width)
+  .attr('height', dimensions.height)
+
+  const bounds = wrapper.append('g')
+  .style('transform', `translate(${dimensions.margin.left}px,  ${dimensions.margin.top}px)`)
 
   // Create scale
 
   const colorScale = d3.scaleOrdinal().domain(groups.map(d=> d.key)).range(groups.map(d => d.color))
 
-  const timeScale = d3.scaleTime().domain(d3.extent(dataFormatted.map(d=> d.year))).range([0,dimensions.boundedWidth])
+  const xScale = d3.scaleTime().domain(d3.extent(dataFormatted.map(d=> d.year))).range([0,dimensions.boundedWidth])
 
-  const areaStack = d3.stack()
-  .keys([groups])
+  const yScale = d3.scaleLinear().domain([0, d3.max(dataFormatted.map(d => d.nominees_total))]).range([dimensions.boundedHeight,0])
+
+  const stack = d3.stack()
+  .keys(groups.map(d=> d.key))
   .order(d3.stackOrderAscending)
   .offset(d3.stackOffsetNone)
 
+  let series = stack(dataFormatted);
+
+
+  // Area generator
+
+  const stackArea = d3.area() // Each array in series rep an ethnicity will be fed to areaGenerator
+  .x(d => xScale(d.data.year))
+  .y0(d => yScale(d[0]))
+  .y1(d => yScale(d[1]))
+  .curve(d3.curveCatmullRom)
+
+  // Draw dataset
+
+
+  const nomineesPath = bounds.append('g')
+  .attr('class', 'stream-paths')
+  .selectAll('path')
+  .data(series)
+  .join('path')
+  .attr('d', d => stackArea(d))
+  .style('fill', d => colorScale(d.key));
 
 };
+
+createViz();
